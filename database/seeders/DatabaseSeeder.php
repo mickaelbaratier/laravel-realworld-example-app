946d860f140562cc0faeedeadcda9bfbaf7cd9fc
mickaelbaratier <mickael.baratier@le-campus-numerique.fr>
2022-09-20 11:53:29 +0200

    ajout du seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
+use Faker\Provider\Lorem;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Psr\Log\NullLogger;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,97 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $id_Rose = DB::table('users')->insertGetId(
+            [
+                'username' => 'Rose',
+                'email' => 'rose@mail.com',
+                'password' => Hash::make('pwd'),
+                'bio' => 'Je voudrais devenir enseignante pour enfants',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        $id_Musonda = DB::table('users')->insertGetId(
+            [
+                'username' => 'Musonda',
+                'email' => 'musonda@mail.com',
+                'password' => Hash::make('pwd2'),
+                'bio' => 'Je songe à devenir infirmière, j’écris mes réflexions',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        );
+
+        $id_articleRose = DB::table('articles')->insertGetId(
+            [
+                'user_id' => $id_Rose,
+                'title' => Str::random(15),
+                'slug' => Str::random(15),
+                'description' => Str::random(15),
+                'body' => Str::random(15),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        );
+        $id_articleMusonda1 = DB::table('articles')->insertGetId(
+            [
+                'user_id' => $id_Musonda,
+                'title' => Str::random(15),
+                'slug' => Str::random(15),
+                'description' => Str::random(15),
+                'body' => Str::random(15),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        );
+        $id_articleMusonda2 = DB::table('articles')->insertGetId(
+            [
+                'user_id' => $id_Musonda,
+                'title' => Str::random(15),
+                'slug' => Str::random(15),
+                'description' => Str::random(15),
+                'body' => Str::random(15),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        );
+
+        DB::table('article_user')->insert([
+                [
+                    'user_id' => $id_Musonda,
+                    'article_id' => $id_articleRose
+                ],
+                [
+                    'user_id' => $id_Rose,
+                    'article_id' => $id_articleMusonda1
+                ],
+                [
+                    'user_id' => $id_Rose,
+                    'article_id' => $id_articleMusonda2
+                ],
+            ]
+        );
+
+        $id_tag = DB::table('tags')->insertGetId(
+            [
+                'name' => 'education'
+            ]
+        );
+
+        DB::table('article_tag')->insert(
+            [
+                'tag_id' => $id_tag,
+                'article_id' => $id_articleRose
+            ]
+        );
+
+        DB::table('comments')->insert(
+            [
+                'user_id' => $id_Musonda,
+                'article_id' => $id_articleRose,
+                'body' => Str::random(30),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        );
     }
 }
