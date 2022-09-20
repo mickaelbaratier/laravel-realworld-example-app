50bc85c37f12ad199b699aa6ada954b23065f333
mickaelbaratier <mickael.baratier@le-campus-numerique.fr>
2022-09-20 12:30:33 +0200

    Creation de test Unitaires.php et nomage des methodes

---
+++ tests/Unit/Unitaires.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Unitaires extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_getRouteKeyNames(){}
+    public function test_articles(){}
+    public function test_favoriteArticles(){}
+    public function test_followers(){}
+    public function test_following(){}
+    public function test_doesUserFollowAnotherUser(){}
+
+}
